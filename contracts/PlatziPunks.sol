e6e38a071d6ae3fd3497c7c8231d64db6483c6b8
gitguti <gitmelgtrrz1@gmail.com>
2022-08-31 23:33:22 -0400

    implement dna of platzipunks

--- contracts/PlatziPunks.sol
+++ contracts/PlatziPunks.sol
@@ -6,8 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "./PlatziPunksDNA.sol";
 
-contract PlatziPunks is ERC721, ERC721Enumerable, Ownable {
+contract PlatziPunks is ERC721, ERC721Enumerable, Ownable, PlatziPunksDNA {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
